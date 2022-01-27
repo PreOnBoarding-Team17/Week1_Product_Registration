eead6af1a2b2b2f18cb8066fcb0c8782766b7166
developjik <developjik@gmail.com>
2022-01-28 02:50:29 +0900

    feat: ProductPeriod 외형 및 CSS 완성

--- src/Components/ProductPeriod/PrductPeriodContainer.jsx
+++ src/Components/ProductPeriod/PrductPeriodContainer.jsx
@@ -2,24 +2,21 @@ import React from "react";
 
 import "./Scss/PrductPeriodContainer.scss";
 
-function PrductPeriodContainer({ title, period, dateType }) {
+function PrductPeriodContainer({ period, dateType }) {
   return (
-    <div className="product-period-container">
-      <div className="product-period-container__title">{title}</div>
-      <div className="product-period-container__input">
-        <div className="product-period-container__input--radio">
-          {period.map((x, idx) => (
-            <label key={idx}>
-              <input type="radio" name={x} />
-              {x}
-            </label>
-          ))}
-        </div>
-        <div className="product-period-container__input--date">
-          <input type={dateType} />
-          <div>~</div>
-          <input type={dateType} />
-        </div>
+    <div className="product-period-container__input">
+      <div className="product-period-container__input--radio">
+        {period.map((x, idx) => (
+          <label key={idx}>
+            <input type="radio" name={x} />
+            {x}
+          </label>
+        ))}
+      </div>
+      <div className="product-period-container__input--date">
+        <input type={dateType} />
+        <div className="date-middle">~</div>
+        <input type={dateType} />
       </div>
     </div>
   );
--- src/Components/ProductPeriod/index.jsx
+++ src/Components/ProductPeriod/index.jsx
@@ -1,26 +1,29 @@
 import React from "react";
 import PrductPeriodContainer from "./PrductPeriodContainer";
 
-import "./Scss/index.scss";
-
 const EXPOSEPERIOD = ["제한 없음", "미노출", "노출 기간 설정"];
 const SALESPERIOD = ["제한 없음", "미판매", "판매 기간 설정"];
 
 function ProductPeriod() {
   return (
-    <div className="product-period">
-      <div className="product-period__title">노출 및 판매기간 설정</div>
-      <PrductPeriodContainer
-        title="상품 노출 기한"
-        period={EXPOSEPERIOD}
-        dateType="date"
-      />
-      <PrductPeriodContainer
-        title="상품 판매 기한"
-        period={SALESPERIOD}
-        dateType="datetime-local"
-      />
-    </div>
+    <section className="gray-box">
+      <div className="title">노출 및 판매기간 설정</div>
+      <div className="menu">
+        <div className="left-menu">상품 노출 기한</div>
+        <div className="right-menu">
+          <PrductPeriodContainer period={EXPOSEPERIOD} dateType="date" />
+        </div>
+      </div>
+      <div className="menu">
+        <div className="left-menu">상품 판매 기한</div>
+        <div className="right-menu">
+          <PrductPeriodContainer
+            period={SALESPERIOD}
+            dateType="datetime-local"
+          />
+        </div>
+      </div>
+    </section>
   );
 }
 
