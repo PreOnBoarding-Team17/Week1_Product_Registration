5d7350d97fb8235c938bdbeaed938bc937d22f00
developjik <developjik@gmail.com>
2022-01-28 02:17:40 +0900

    feat: CSS 변경 및 추가

--- src/Components/ProductPeriod/index.jsx
+++ src/Components/ProductPeriod/index.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import PrductPeriodContainer from "./PrductPeriodContainer";
 
+import "./Scss/index.scss";
+
 const EXPOSEPERIOD = ["제한 없음", "미노출", "노출 기간 설정"];
 const SALESPERIOD = ["제한 없음", "미판매", "판매 기간 설정"];
 
 function ProductPeriod() {
   return (
-    <>
+    <div className="product-period">
+      <div className="product-period__title">노출 및 판매기간 설정</div>
       <PrductPeriodContainer
         title="상품 노출 기한"
         period={EXPOSEPERIOD}
@@ -17,7 +20,7 @@ function ProductPeriod() {
         period={SALESPERIOD}
         dateType="datetime-local"
       />
-    </>
+    </div>
   );
 }
 
