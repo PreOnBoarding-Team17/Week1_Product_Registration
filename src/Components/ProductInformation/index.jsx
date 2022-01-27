b2de31e1b0d3d722c5c3ab0830f0c2ef25bffbdf
som-syom <dhyeon@student.42seoul.kr>
2022-01-27 21:15:27 +0900

    Feat - 공통으로 쓸 scss 수정, 샘플 추가

--- src/Components/ProductInformation/index.jsx
+++ src/Components/ProductInformation/index.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function ProductInformation() {
   return (
     <section className="gray-box">
-      <div className="title">상품 기본 정보</div>
+      <div className="title">gray-box</div>
       <div className="menu">
-        <div className="left-menu">카테고리 *</div>
-        <div className="right-menu">카테고리명</div>
+        <div className="left-menu">left-menu</div>
+        <div className="right-menu">right-menu</div>
       </div>
       <div className="menu">
         <div className="left-menu">카테고리 *</div>
