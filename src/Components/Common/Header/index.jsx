7ae8274e7219b51a4bd1e8e83ada15ab842154c9
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-01-28 02:12:24 +0900

    feat: 헤더 구현

--- src/Components/Common/Header/index.jsx
+++ src/Components/Common/Header/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import "Components/Common/Header/scss/Header.scss";
 
-function Header() {
-  return <div>Header</div>;
+export default function Header() {
+  return (
+    <header className="header__container">
+      <div className="header__box-blank"></div>
+      <div className="header__box">
+        <h1>상품 등록</h1>
+        <button type="submit">저장하기</button>
+      </div>
+    </header>
+  );
 }
-
-export default Header;
