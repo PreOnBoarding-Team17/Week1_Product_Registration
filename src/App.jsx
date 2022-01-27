d478f88a6146e4d98deef646c4158d80e1b39196
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-01-28 01:06:10 +0900

    feat: reset.scss 적용

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navigation from "Components/Common/Navigation";
 import ProductRegister from "Pages/ProductRegister";
 import "Utils/Styles/common.scss";
+import "Utils/Styles/_reset.scss";
 
 function App() {
   return (
