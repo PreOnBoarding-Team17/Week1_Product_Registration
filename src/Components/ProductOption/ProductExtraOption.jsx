6dd7092040e5c1711d31f47c2eaf4a32611b6517
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-01-29 05:43:10 +0900

    feat: ProductExtraOption -> Context API 추가

--- src/Components/ProductOption/ProductExtraOption.jsx
+++ src/Components/ProductOption/ProductExtraOption.jsx
@@ -1,32 +1,64 @@
-import React from "react";
+import React, { useContext } from "react";
+import { FormContext } from "FormContextAPI/FormContextAPI";
 import DeleteButton from "Utils/Button/DeleteButton";
 import "Components/ProductOption/scss/ProductOptionList.scss";
 
-export default function ProductExtraOption({ extraOption, onClick }) {
+export default function ProductExtraOption({
+  index,
+  contentsIndex,
+  extraIndex,
+}) {
+  const context = useContext(FormContext).optionData;
+  const state = context.state;
+  const setState = context.setState;
+  const extraOption =
+    state[index].optionContents[contentsIndex].extraOption[extraIndex];
+
   return (
     <>
-      {extraOption.length !== 0 &&
-        extraOption.map((element, index) => (
-          <article key={index} id={element} className="option-list__box">
-            <div className="option-list__connect"></div>
-            <div className="option-list__element mid">
-              <input
-                className="option-list__input"
-                type="text"
-                placeholder="추가 옵션명 (필수)"
-              />
-            </div>
-            <div className="option-list__element mid">
-              <input
-                className="option-list__input"
-                type="number"
-                placeholder="추가 옵션 정상가 (필수)"
-              />
-              <p>원</p>
-            </div>
-            <DeleteButton onClick={onClick} />
-          </article>
-        ))}
+      <article className="option-list__box">
+        <div className="option-list__connect"></div>
+        <div className="option-list__element mid">
+          <input
+            className="option-list__input"
+            type="text"
+            placeholder="추가 옵션명 (필수)"
+            value={extraOption.name}
+            onChange={(event) => {
+              const updatedState = [...state];
+              updatedState[index].optionContents[contentsIndex].extraOption[
+                extraIndex
+              ].name = event.target.value;
+              setState(updatedState);
+            }}
+          />
+        </div>
+        <div className="option-list__element mid">
+          <input
+            className="option-list__input"
+            type="number"
+            placeholder="추가 옵션 정상가 (필수)"
+            value={extraOption.price}
+            onChange={(event) => {
+              const updatedState = [...state];
+              updatedState[index].optionContents[contentsIndex].extraOption[
+                extraIndex
+              ].price = event.target.value;
+              setState(updatedState);
+            }}
+          />
+          <p>원</p>
+        </div>
+        <DeleteButton
+          onClick={() => {
+            const updatedState = [...state];
+            updatedState[index].optionContents[
+              contentsIndex
+            ].extraOption.splice(extraIndex, 1);
+            setState(updatedState);
+          }}
+        />
+      </article>
     </>
   );
 }
