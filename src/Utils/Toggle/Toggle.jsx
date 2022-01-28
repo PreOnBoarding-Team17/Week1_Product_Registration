8e44346cb812f1aa8ed96dad0b5f38b6b6956260
developjik <developjik@gmail.com>
2022-01-28 10:49:10 +0900

    feat: Toggle Button 구현 완료

---
+++ src/Utils/Toggle/Toggle.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import './Toggle.scss';
+
+function Toggle() {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <div className="toggle-container">
+      <label className="toggle">
+        <input
+          type="checkbox"
+          onChange={() => {
+            setToggle(!toggle);
+          }}
+        />
+        <span className="slider" />
+      </label>
+    </div>
+  );
+}
+
+export default Toggle;
