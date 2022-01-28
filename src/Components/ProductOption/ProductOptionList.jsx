ddfe5078845c64376652152b11ff895ebc0b318c
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-01-28 21:24:09 +0900

    feat: 옵션 리스트, 옵션 전체 추가 및 삭제 기능 적용

--- src/Components/ProductOption/ProductOptionList.jsx
+++ src/Components/ProductOption/ProductOptionList.jsx
@@ -4,67 +4,72 @@ import { handleAddOption, handleDeleteOption } from "Utils/OptionSetting";
 import ProductExtraOption from "Components/ProductOption/ProductExtraOption";
 import "Components/ProductOption/scss/ProductOptionList.scss";
 
-export default function ProductOptionList() {
+export default function ProductOptionList({ optionList, onClick }) {
   const [extraOption, setExtraOption] = useState([]);
 
   return (
-    <section className="option-list__container">
-      <article className="option-list__box flex-end">
-        <DeleteButton />
-      </article>
-      <article className="option-list__box">
-        <input
-          className="option-list__input long"
-          type="text"
-          placeholder="옵션명을 입력해 주세요.(필수)"
-        />
-      </article>
-      <article className="option-list__box">
-        <div className="option-list__element">
-          <input
-            className="option-list__input"
-            type="number"
-            placeholder="상품 정상가 (필수)"
-          />
-          <p>원</p>
-        </div>
-        <div className="option-list__element">
-          <p>10%</p>
-        </div>
-        <div className="option-list__element">
-          <input
-            className="option-list__input"
-            type="number"
-            placeholder="상품 판매가 (필수)"
-          />
-          <p>원</p>
-        </div>
-        <div className="option-list__element">
-          <input
-            className="option-list__input"
-            type="number"
-            placeholder="재고 (필수)"
-          />
-          <p>개</p>
-        </div>
-        <select className="option-list__select" name="과세 여부" id="">
-          <option value="비과세">비과세</option>
-          <option value="과세">과세</option>
-        </select>
-      </article>
-      <ProductExtraOption
-        extraOption={extraOption}
-        onClick={() => handleDeleteOption(setExtraOption)}
-      />
-      <article className="option-list__box">
-        <button
-          className="option-list__add"
-          onClick={() => handleAddOption(extraOption, setExtraOption)}
-        >
-          <p className="option-list__add-plus">+</p>
-          <p>추가 옵션 상품 추가</p>
-        </button>
-      </article>
-    </section>
+    <>
+      {optionList.length !== 0 &&
+        optionList.map((element) => (
+          <section key={element} className="option-list__container">
+            <article className="option-list__box flex-end">
+              <DeleteButton onClick={onClick} />
+            </article>
+            <article className="option-list__box">
+              <input
+                className="option-list__input long"
+                type="text"
+                placeholder="옵션명을 입력해 주세요.(필수)"
+              />
+            </article>
+            <article className="option-list__box">
+              <div className="option-list__element">
+                <input
+                  className="option-list__input"
+                  type="number"
+                  placeholder="상품 정상가 (필수)"
+                />
+                <p>원</p>
+              </div>
+              <div className="option-list__element">
+                <p>10%</p>
+              </div>
+              <div className="option-list__element">
+                <input
+                  className="option-list__input"
+                  type="number"
+                  placeholder="상품 판매가 (필수)"
+                />
+                <p>원</p>
+              </div>
+              <div className="option-list__element">
+                <input
+                  className="option-list__input"
+                  type="number"
+                  placeholder="재고 (필수)"
+                />
+                <p>개</p>
+              </div>
+              <select className="option-list__select" name="과세 여부" id="">
+                <option value="비과세">비과세</option>
+                <option value="과세">과세</option>
+              </select>
+            </article>
+            <ProductExtraOption
+              extraOption={extraOption}
+              onClick={() => handleDeleteOption(setExtraOption)}
+            />
+            <article className="option-list__box">
+              <button
+                className="option-list__add"
+                onClick={() => handleAddOption(extraOption, setExtraOption)}
+              >
+                <p className="option-list__add-plus">+</p>
+                <p>추가 옵션 상품 추가</p>
+              </button>
+            </article>
+          </section>
+        ))}
+    </>
   );
 }
--- src/Components/ProductOption/index.jsx
+++ src/Components/ProductOption/index.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { handleAddOption, handleDeleteOption } from "Utils/OptionSetting";
 import ProductOptionSet from "Components/ProductOption/ProductOptionSet";
 import "Components/ProductOption/scss/ProductOption.scss";
 
 export default function ProductOption() {
+  const [optionSet, setOptionSet] = useState([]);
   return (
     <section className="gray-box">
       <div className="option__title-box">
         <p className="option__title">상품 옵션*</p>
-        <button className="option__button">+ 옵션 세트 추가</button>
+        <button
+          className="option__button"
+          onClick={() => handleAddOption(optionSet, setOptionSet)}
+        >
+          + 옵션 세트 추가
+        </button>
       </div>
       <div className="option__list">
-        <ProductOptionSet />
+        <ProductOptionSet
+          optionSet={optionSet}
+          onClick={() => handleDeleteOption(setOptionSet)}
+        />
       </div>
     </section>
   );
