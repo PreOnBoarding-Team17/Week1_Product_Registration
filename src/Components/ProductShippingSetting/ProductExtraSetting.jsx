12f5a0017690191f71dbd58ffeb1db37e3c69d43
developjik <developjik@gmail.com>
2022-01-28 16:20:35 +0900

    refactor: ProductShippingSetting 샘플과 동일하게 구현

--- src/Components/ProductShippingSetting/ProductExtraSetting.jsx
+++ src/Components/ProductShippingSetting/ProductExtraSetting.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Toggle from "Utils/Toggle/Toggle";
 
+import "Components/ProductShippingSetting/scss/ProductExtraSetting.scss";
+
 function ProdectExtraSetting({ title, option }) {
   return (
     <section className="gray-box">
       <div className="title">{title}</div>
       <div className="menu">
         <div className="left-menu">{option}</div>
-        <div className="right-menu">
+        <div className="right-menu toggle-contain">
           <Toggle />
         </div>
       </div>
--- src/Components/ProductShippingSetting/index.jsx
+++ src/Components/ProductShippingSetting/index.jsx
@@ -11,7 +11,7 @@ const EXTRASETTING_2 = ["기타 설정", "감사카드 설정"];
 
 function ProductShippingSetting() {
   return (
-    <>
+    <div className="product-shipping-setting-container">
       <section className="gray-box">
         <div className="title">상품 배송 설정</div>
         <div className="menu">
@@ -20,40 +20,53 @@ function ProductShippingSetting() {
             <br />
             출발일 지정
           </div>
-          <div className="right-menu">
+          <div className="right-menu toggle-contain">
             <Toggle />
           </div>
         </div>
+
+        <div className="menu border-top-none">
+          <div className="left-menu double-text">방문 수령</div>
+          <div className="right-menu toggle-contain">
+            <Toggle />
+          </div>
+        </div>
+
         <div className="menu">
           <div className="left-menu double-text">
-            방문 수령
-            <br />
-            <br />
-            <br />선 주문
+            선 주문
             <br />
             예약배송
           </div>
-          <div className="right-menu">
-            <Toggle />
-            <br />
-            <br />
-            <br />
+          <div className="right-menu toggle-contain">
             <Toggle />
             <div className="shipping-time">
               <div className="shipping-time--detail">
-                주문시간
-                <input type="datetime-local" />
+                주문 시간
+                <input
+                  type="datetime-local"
+                  className="shipping-time--detail__datetime"
+                />
                 ~
-                <input type="datetime-local" />
+                <input
+                  type="datetime-local"
+                  className="shipping-time--detail__datetime"
+                />
               </div>
               <div className="shipping-time--detail">
                 <div>
-                  새벽배송
-                  <input type="date" />
+                  새벽 배송
+                  <input
+                    type="date"
+                    className="shipping-time--detail__datetime shipping-time--detail__date"
+                  />
                 </div>
                 <div>
-                  일반배송
-                  <input type="date" />
+                  일반 배송
+                  <input
+                    type="date"
+                    className="shipping-time--detail__datetime shipping-time--detail__date"
+                  />
                 </div>
               </div>
             </div>
@@ -69,7 +82,7 @@ function ProductShippingSetting() {
         title={EXTRASETTING_2[0]}
         option={EXTRASETTING_2[1]}
       />
-    </>
+    </div>
   );
 }
 
