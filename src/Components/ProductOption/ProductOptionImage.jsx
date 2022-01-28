7eb4a9bb1151f8cb4562143c0a2b1563503c9659
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-01-28 21:51:09 +0900

    feat: 이미지 업로드시 프리뷰 기능 구현

--- src/Components/ProductOption/ProductOptionImage.jsx
+++ src/Components/ProductOption/ProductOptionImage.jsx
@@ -1,23 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "Components/ProductOption/scss/ProductOptionImage.scss";
 
 export default function ProductOptionImage() {
   const [image, setImage] = useState("");
-  const handleImage = () => {
-    setImage();
+  const inputRef = useRef(null);
+
+  const handleAddImage = (event) => {
+    if (event.target.files) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const previewImage = document.getElementById("preview-image");
+        previewImage.src = event.target.result;
+      };
+      reader.readAsDataURL(event.target.files[0]);
+      setImage(event.target.files[0].name);
+    }
   };
+
+  const handleClick = () => {
+    inputRef.current.click();
+    inputRef.current.value = null;
+  };
+
   return (
     <section className="option-image__container">
-      <div className="option-image__input-box">
-        <input
-          className="option-image__input"
-          type="file"
-          accept="image/*"
-          onChange={handleImage}
+      {image.length !== 0 ? (
+        <img
+          className="option-image__preview-image"
+          id="preview-image"
+          src=""
+          alt="프리뷰 이미지"
         />
-        <button className="option-image__button">+ 이미지 첨부</button>
-      </div>
-      {image.length !== 0 ? <p>{image}</p> : ""}
+      ) : (
+        <div className="option-image__input-box">
+          <input
+            className="option-image__input"
+            type="file"
+            accept="image/*"
+            onChange={handleAddImage}
+            ref={inputRef}
+          />
+          <button className="option-image__button" onClick={handleClick}>
+            + 이미지 첨부
+          </button>
+        </div>
+      )}
     </section>
   );
 }
