b2dd36ee014bc2bdde3f732c7b0c6d0fc02788e8
som-syom <dhyeon@student.42seoul.kr>
2022-01-28 16:55:48 +0900

    feat: 필터 태그 input focus 일때 검색결과창 보이도록 구현

--- src/Components/ProductInformation/ProductFilterTag.jsx
+++ src/Components/ProductInformation/ProductFilterTag.jsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "Components/ProductInformation/scss/ProductFilterTag.scss";
 
 function ProductFilterTag() {
+  const [filterSearch, setFilterSearch] = useState(false);
+
   return (
-    <div className="filter-tag__container">
-      <input
-        type="text"
-        className="filter-tag__input"
-        placeholder="필터태그를 검색해 주세요."
-      />
-      <button className="filter-tag__btn">검색</button>
-    </div>
+    <>
+      <div className="filter-tag__container">
+        <input
+          type="text"
+          className="filter-tag__input"
+          placeholder="필터태그를 검색해 주세요."
+          onFocus={() => setFilterSearch(true)}
+          onBlur={() => setFilterSearch(false)}
+        />
+        <button className="filter-tag__btn">검색</button>
+      </div>
+      {filterSearch && (
+        <div className="filter-tag__search-result">
+          <ul className="filter-tag__search-result__list">
+            <li className="filter-tag__search-result__item">필터태그</li>
+            <li className="filter-tag__search-result__item">필터태그</li>
+            <li className="filter-tag__search-result__item">필터태그asdf</li>
+            <li className="filter-tag__search-result__item">필터태그a</li>
+            <li className="filter-tag__search-result__item">필터태그</li>
+            <li className="filter-tag__search-result__item">필터태그</li>
+            <li className="filter-tag__search-result__item">필터태그asfd</li>
+            <li className="filter-tag__search-result__item">필터태그</li>
+            <li className="filter-tag__search-result__item">필터태그</li>
+            <li className="filter-tag__search-result__item">필터태그asdf</li>
+            <li className="filter-tag__search-result__item">필터태그</li>
+            <li className="filter-tag__search-result__item">필터태그</li>
+          </ul>
+        </div>
+      )}
+    </>
   );
 }
 
