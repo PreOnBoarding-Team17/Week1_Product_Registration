b5a811f2c205718b2e09f0148bf2711aa1785104
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-01-29 02:18:28 +0900

    feat: ProductOptionSet -> Context API 추가

--- src/Components/ProductOption/ProductOptionSet.jsx
+++ src/Components/ProductOption/ProductOptionSet.jsx
@@ -1,40 +1,35 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
+import { FormContext } from "FormContextAPI/FormContextAPI";
 import DeleteButton from "Utils/Button/DeleteButton";
-import { handleAddOption, handleDeleteOption } from "Utils/OptionSetting";
 import ProductOptionImage from "./ProductOptionImage";
 import ProductOptionList from "Components/ProductOption/ProductOptionList";
 import "Components/ProductOption/scss/ProductOptionSet.scss";
 
-export default function ProductOptionSet({ optionSet, onClick }) {
-  const [optionList, setOptionList] = useState([0]);
+export default function ProductOptionSet() {
+  const context = useContext(FormContext).optionData;
+  const state = context.state;
+  const setState = context.setState;
+
   return (
     <>
-      {optionSet.length !== 0 ? (
-        optionSet.map((element) => (
-          <section key={element} className="option-set__container">
-            <div className="option-set__button-box">
-              <DeleteButton onClick={onClick} />
-            </div>
-            <div className="option-set__wrapper">
-              <ProductOptionImage number={element} />
-              <ProductOptionList
-                optionList={optionList}
-                onClick={() => handleDeleteOption(setOptionList)}
-              />
-              <button
-                className="option-set__add-button"
-                onClick={() => handleAddOption(optionList, setOptionList)}
-              >
-                + 옵션 추가
-              </button>
-            </div>
-          </section>
-        ))
-      ) : (
-        <div className="option-set__empty">
-          <p>옵션세트를 추가하여 옵션을 구성해 주세요.</p>
-        </div>
-      )}
+      {state.map((element, index) => (
+        <section key={index} className="option-set__container">
+          <div className="option-set__button-box">
+            <DeleteButton
+              onClick={() => {
+                const originalState = [...state];
+                originalState.splice(index, 1);
+                setState(originalState);
+              }}
+            />
+          </div>
+          <div className="option-set__wrapper">
+            <ProductOptionImage index={index} />
+            <ProductOptionList index={index} />
+            <button className="option-set__add-button">+ 옵션 추가</button>
+          </div>
+        </section>
+      ))}
     </>
   );
 }
