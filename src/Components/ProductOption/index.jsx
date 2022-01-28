bde2ee0285a7faf7d2ca004ea4b7dc071121c35e
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-01-29 02:17:56 +0900

    feat: ProductOption -> Context API 추가

--- src/Components/ProductOption/index.jsx
+++ src/Components/ProductOption/index.jsx
@@ -1,27 +1,41 @@
-import React, { useState } from "react";
-import { handleAddOption, handleDeleteOption } from "Utils/OptionSetting";
+import React, { useContext } from "react";
+import { FormContext } from "FormContextAPI/FormContextAPI";
 import ProductOptionSet from "Components/ProductOption/ProductOptionSet";
 import "Components/ProductOption/scss/ProductOption.scss";
 
 export default function ProductOption() {
-  const [optionSet, setOptionSet] = useState([]);
+  const context = useContext(FormContext).optionData;
+  const state = context.state;
+  const setState = context.setState;
 
   return (
     <section className="gray-box">
       <div className="option__title-box">
         <p className="option__title">상품 옵션*</p>
         <button
+          type="button"
           className="option__button"
-          onClick={() => handleAddOption(optionSet, setOptionSet)}
+          onClick={() => {
+            setState([
+              ...state,
+              {
+                optionImage: [],
+                optionContents: [
+                  {
+                    optionName: "",
+                    optionList: ["", "", "", "비과세"],
+                    extraOption: [],
+                  },
+                ],
+              },
+            ]);
+          }}
         >
           + 옵션 세트 추가
         </button>
       </div>
       <div className="option__list">
-        <ProductOptionSet
-          optionSet={optionSet}
-          onClick={() => handleDeleteOption(setOptionSet)}
-        />
+        <ProductOptionSet />
       </div>
     </section>
   );
