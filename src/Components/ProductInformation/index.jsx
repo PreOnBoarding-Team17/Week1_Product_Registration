12ea3f0bf29d6c203056c787cc5e3d63a83b14c4
som-syom <dhyeon@student.42seoul.kr>
2022-01-28 14:10:11 +0900

    feat: 상품 정보 기본 틀 구현
    - 이미지 첨부 영역 제외한 기본 영역 틀 구현

--- src/Components/ProductInformation/index.jsx
+++ src/Components/ProductInformation/index.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ProductCategory from "Components/ProductInformation/ProductCategory";
 import ProductFilterTag from "./ProductFilterTag";
+import ProductName from "./ProductName";
+import ProductCompsitionInfo from "./ProductCompsitionInfo";
+import "Components/ProductInformation/scss/ProductInformation.scss";
 
 function ProductInformation() {
   return (
@@ -20,11 +23,17 @@ function ProductInformation() {
       </div>
       <div className="menu">
         <div className="left-menu">상품명 *</div>
-        <div className="right-menu"></div>
+        <div className="right-menu name-menu">
+          <ProductName />
+        </div>
+        <div className="left-menu">상품 코드</div>
+        <div className="right-menu name-menu">code</div>
       </div>
       <div className="menu">
         <div className="left-menu">상품 구성 소개 정보 *</div>
-        <div className="right-menu"></div>
+        <div className="right-menu">
+          <ProductCompsitionInfo />
+        </div>
       </div>
       <div className="menu">
         <div className="left-menu">상품 썸네일</div>
