2f29d4b0a65ff3f0e3960092e5cfd43b33921c6f
developjik <developjik@gmail.com>
2022-01-28 23:12:21 +0900

    refactor: Toggle 로직 수정

--- src/Utils/Toggle/Toggle.jsx
+++ src/Utils/Toggle/Toggle.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useEffect, useContext, useState } from "react";
+import { FormContext } from "FormContextAPI/FormContextAPI";
 import "Utils/Toggle/Toggle.scss";
 
 function Toggle({ name, value, onChange }) {
+  const [checked, setChecked] = useState(false);
+  const { inputs } = useContext(FormContext);
+
+  useEffect(() => {
+    if (inputs[name] === "true") {
+      setChecked(true);
+    } else {
+      setChecked(false);
+    }
+  }, [inputs[name]]);
+
   return (
     <div className="toggle-container">
       <label className="toggle">
-        <input type="checkbox" name={name} value={value} onChange={onChange} />
+        {value === "true" ? (
+          <input
+            type="checkbox"
+            name={name}
+            value={value}
+            onClick={onChange}
+            // onChange={onChange}
+            checked={checked}
+          />
+        ) : (
+          <input
+            type="checkbox"
+            name={name}
+            value={value}
+            onClick={onChange}
+            // onChange={onChange}
+            checked={checked}
+          />
+        )}
 
         <span className="slider" />
       </label>
